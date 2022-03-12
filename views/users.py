9faa7834949c270c31a02b55c26ae721aa6e57b6
NurmiQ <v-kogan@rambler.ru>
2022-03-12 20:26:43 +0300

    step 2 is done

---
+++ views/users.py
@@ -0,0 +1,51 @@
+from flask_restx import Resource, Namespace
+from flask import request
+from models import User, UserSchema
+from setup_db import db
+
+user_ns = Namespace('users')
+
+
+@user_ns.route('/')
+class GenresView(Resource):
+    def get(self):
+        rs = db.session.query(User).all()
+        res = UserSchema(many=True).dump(rs)
+        return res, 200
+
+
+    def post(self):
+        req_json = request.json
+        ent = User(**req_json)
+
+        db.session.add(ent)
+        db.session.commit()
+        return "", 201, {"location": f"/movies/{ent.id}"}
+
+
+@user_ns.route('/<int:uid>')
+class UserView(Resource):
+    def get(self, uid):
+        r = db.session.query(User).get(uid)
+        sm_d = UserSchema().dump(r)
+        return sm_d, 200
+
+
+    def put(self, uid):
+        user = db.session.query(User).get(uid)
+        req_json = request.json
+        user.username = req_json.get("username")
+        user.password = req_json.get("password")
+        user.role = req_json.get("role")
+        db.session.add(user)
+        db.session.commit()
+        return "", 204
+
+
+    def delete(self, uid):
+        user = db.session.query(User).get(uid)
+
+        db.session.delete(user)
+        db.session.commit()
+        return "", 204
+
