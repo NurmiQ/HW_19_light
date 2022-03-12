5baf11898e893e3b56463b830f099c70f80b00f7
NurmiQ <v-kogan@rambler.ru>
2022-03-12 20:54:25 +0300

    step 3 is done

--- views/directors.py
+++ views/directors.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-
+from flask import request
 from models import Director, DirectorSchema
 from setup_db import db
 
@@ -13,6 +13,13 @@ class DirectorsView(Resource):
         res = DirectorSchema(many=True).dump(rs)
         return res, 200
 
+    def post(self):
+        req_json = request.json
+        ent = Director(**req_json)
+
+        db.session.add(ent)
+        db.session.commit()
+        return "", 201, {"location": f"/directors/{ent.id}"}
 
 @director_ns.route('/<int:rid>')
 class DirectorView(Resource):
@@ -20,3 +27,19 @@ class DirectorView(Resource):
         r = db.session.query(Director).get(rid)
         sm_d = DirectorSchema().dump(r)
         return sm_d, 200
+
+
+    def put(self, rid):
+        director = db.session.query(Director).get(rid)
+        req_json = request.json
+        director.name = req_json.get("name")
+        db.session.add(director)
+        db.session.commit()
+        return "", 204
+
+    def delete(self, rid):
+        director = db.session.query(Director).get(rid)
+
+        db.session.delete(director)
+        db.session.commit()
+        return "", 204
--- views/genres.py
+++ views/genres.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-
+from flask import request
 from models import Genre, GenreSchema
 from setup_db import db
 
@@ -13,6 +13,14 @@ class GenresView(Resource):
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    def post(self):
+        req_json = request.json
+        ent = Genre(**req_json)
+
+        db.session.add(ent)
+        db.session.commit()
+        return "", 201, {"location": f"/genres/{ent.id}"}
+
 
 @genre_ns.route('/<int:rid>')
 class GenreView(Resource):
@@ -20,3 +28,18 @@ class GenreView(Resource):
         r = db.session.query(Genre).get(rid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
+
+    def put(self, rid):
+        genre = db.session.query(Genre).get(rid)
+        req_json = request.json
+        genre.name = req_json.get("name")
+        db.session.add(genre)
+        db.session.commit()
+        return "", 204
+
+    def delete(self, rid):
+        genre = db.session.query(Genre).get(rid)
+
+        db.session.delete(genre)
+        db.session.commit()
+        return "", 204
