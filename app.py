6ddc672fdda38f6ce0271e757662d5b28beebc3c
NurmiQ <v-kogan@rambler.ru>
2022-03-12 23:01:32 +0300

    step 4

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_restx import Api
 
 from config import Config
+from models import User
 from setup_db import db
+from views.auth import auth_ns
 from views.directors import director_ns
 from views.genres import genre_ns
 from views.movies import movie_ns
@@ -21,6 +23,7 @@ def register_extensions(app):
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
+    api.add_namespace(auth_ns)
     create_data(app, db)
 
 
